51b6d70e8273e55370e45215b92b1287e3531611
jjac91 <denjand91@gmail.com>
2025-03-30 02:22:41 -0400

    jenkins test

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const server = http.createServer((req, res) => {
   res.end("Hello, World! from Kubernetes setup!\n");
 });
 server.listen(3000, () => {
-  console.log("Server is running on port 3000!");
+  console.log("Server is running on port 3000. Jenkins Test");
 });
